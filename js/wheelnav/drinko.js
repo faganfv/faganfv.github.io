fc5b1343e9af635bc56dada065d0db1bb2b034dd
Fred Fagan <faganfv@gmail.com>
2017-04-25 23:29:22 -0400

    Clean up code and start notes

---
+++ js/wheelnav/drinko.js
@@ -0,0 +1,20 @@
+var piemenu
+
+window.onload = function () {
+
+  piemenu = new wheelnav('piemenu', null, 550, 450);
+  piemenu.markerEnable = true;
+  piemenu.markerPathFunction = markerPath().TriangleMarker;
+  piemenu.clockwise = false;
+  piemenu.sliceInitPathFunction = piemenu.slicePathFunction;
+  piemenu.initPercent = .1;
+  piemenu.titleSelectedAttr = { fill: "#111" };
+  piemenu.titleRotateAngle = 180;
+  piemenu.createWheel();
+  spin();
+};
+
+function spin() {
+  piemenu.rotateRoundCount = 15;
+  piemenu.navigateWheel(Math.floor(Math.random() * 6));
+}
